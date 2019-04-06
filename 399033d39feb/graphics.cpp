2bf9e8a64550f3eab4058eb8ef6ae0b6983a15b6
Aditya Jituri <adi.jituri@gmail.com>
2019-04-06 17:31:14 -0400

    B

--- 399033d39feb/graphics.cpp
+++ 399033d39feb/graphics.cpp
@@ -53,7 +53,7 @@ void draw_nothing(int u, int v)
     };
     for (int i = 0; i <= 10; i++){
         for (int j = 0; j <= 10; j++) {
-            uLCD.pixel(u + i, v + j, dirtblock[i][j]);
+            uLCD.pixel(u + i, v + j, dirtBlock[i][j]);
         }
     }
 }
